1f0e1d86987af4939cfa8c8a698af4cfcee95e7a
ddc <70716365+daandecorte@users.noreply.github.com>
2024-09-21 14:02:57 +0200

    pipes
    pipes renderen nu

--- obstakel.js
+++ obstakel.js
@@ -6,22 +6,12 @@ class obstakel {
         this.vx = vx;
     }
     draw(ctx) {
-        ctx.beginPath();
-        ctx.rect(this.x, this.height, 100, 650);
-        ctx.rect(this.x, 0, 100, this.height-200);
-        ctx.fillStyle = "black";
-        ctx.fill();
-
-        // let img1 = new Image();
-        // let img2 = new Image();
-        // img1.width = 100;
-        // img1.height = 650;
-        // img1.src = "pipe.png";
-        // ctx.drawImage(img1, this.x, this.height);
-        // img2.width = 100;
-        // img2.height = this.height-200;
-        // img2.src = "pipe2.png";
-        // ctx.drawImage(img2, this.x, 0);
+        let img1 = new Image();
+        let img2 = new Image();
+        img1.src = "pipe.png";
+        ctx.drawImage(img1, this.x, this.height);
+        img2.src = "pipe2.png";
+        ctx.drawImage(img2, this.x, this.height-800);
 
     }
     move(canvas) {
